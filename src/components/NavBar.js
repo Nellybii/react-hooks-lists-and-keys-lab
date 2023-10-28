a78c164cfef780dac939e986f3fc93e77ff9fb1d
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-28 11:31:59 +0300

    updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import Home from "./Home"
+import About from "./About";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+        {links.map((link, index) => (
+        <a key={index} href={`#${link.toLowerCase()}`}>
+          {link.toLowerCase()}
+        </a>
+      ))}
+
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
   return (
@@ -6,6 +7,13 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+  {technologies.map((technology, index)=>(
+              <span key={index} className="technology">
+                {technology}
+                </span>
+            )
+            )
+  }
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
